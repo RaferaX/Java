73b50be4804ed8345507970c0dbd79d2aeef2a3c
lab <lab@lab>
2024-04-09 09:18:44 -0300

    segundo commit

---
+++ Pilha.java
@@ -0,0 +1,10 @@
+public class Pilha<T> extends EstruturaEstatica<T> {
+    
+    public Pilha(){
+        super();
+    }
+
+    public Pilha(int capacidade) {
+        super(capacidade);
+    }
+}
---
+++ Teste.java
@@ -0,0 +1,9 @@
+public class Teste {
+    
+    public static void main(String[] args) {
+        Pilha<Integer> pilha = new Pilha<Integer>();
+
+        System.out.println(pilha);
+        System.out.println(pilha.tamanho());
+    }
+}
